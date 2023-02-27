d3e1421d5f5f7bb3530ab826c966a409d9a2eeb4
Ruan Carolino Segrini <102704092+RuanSegrini@users.noreply.github.com>
2023-02-27 09:44:31 -0300

    Update Pares.consecutivos.c

--- Pares.consecutivos.c
+++ Pares.consecutivos.c
@@ -9,16 +9,23 @@ int x, soma;
 printf("Digite um numero inteiro: ");
 scanf("%d", &x);
 
-soma = 0;
-
-
 while (x != 0){
 
 
-    scanf("%d", &x);
-    soma = x + soma;
 
-    printf("SOMA = %d", soma);
+if(x % 2 == 0 ){
+    soma = x +(x + 2)+(x + 4)+(x+6)+(x+8);
+      printf("Soma = %d", soma);
+       
+}
+else{
+    soma = (x + 1)+(x + 3)+(x + 5)+(x+7)+(x+9);
+      printf("Soma = %d", soma);
+}
+
+printf("\nDigite um numero inteiro: ");
+scanf("%d", &x);
+  
 }
 
 
